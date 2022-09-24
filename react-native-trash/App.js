ba5b47d0cb4abfc2ebc3a20bcb9a3d993be3b1ab
filiprejmus <rejmusfilip@gmail.com>
2022-09-24 20:40:37 +0200

    Navigation implementation and first components

--- react-native-trash/App.js
+++ react-native-trash/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native";
+import Homescreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your Trashcollection!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Home" component={Homescreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ react-native-trash/screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Pressable, View, Text, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Entypo } from "@expo/vector-icons";
+
+export default function Homescreen() {
+	return (
+		<SafeAreaView style={styles.safeAreaContainer}>
+			<View style={styles.container}>
+				<Pressable
+					style={({ pressed }) => [
+						{
+							opacity: pressed ? 0.3 : 1,
+						},
+						styles.button,
+					]}
+				>
+					<View style={styles.row}>
+						<Text style={styles.buttonText}>Report Trash!</Text>
+						<Entypo name="trash" size={18} color="white" />
+					</View>
+				</Pressable>
+				<StatusBar style="auto" />
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	button: {
+		backgroundColor: "salmon",
+		padding: 10,
+		borderRadius: 10,
+	},
+	buttonText: {
+		fontSize: 18,
+		paddingRight: 8,
+		color: "white",
+	},
+	row: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 8,
+	},
+	safeAreaContainer: {
+		flex: 1,
+	},
+});
