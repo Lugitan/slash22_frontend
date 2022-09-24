4c2a7bb6f586bd70fd443cf42667595fce998999
Gilles Tanson <gillestanson@googlemail.com>
2022-09-24 23:54:50 +0200

    pickupscreen v2

--- react-native-trash/screens/PickupScreen.js
+++ react-native-trash/screens/PickupScreen.js
@@ -1,31 +1,76 @@
 import React from "react";
-import {
-	SafeAreaView,
-	ScrollView,
-	VStack,
-	Center,
-	Text,
-	useTheme,
-	Heading,
-	NativeBaseProvider,
-	Box,
-} from "native-base";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { ScrollView, AspectRatio, Center, Stack, useTheme, Heading, NativeBaseProvider, Box, Divider, Image } from "native-base";
 import TrashItem from "../components/TrashItem";
 
 export default function PickupScreen() {
 	const { colors } = useTheme();
 	return (
-		<NativeBaseProvider>
-			<Center flex={1} px="3">
-				<Box>
+		<SafeAreaView style={styles.areaView}>
+			<NativeBaseProvider>
+				<Center mt="45px" h="100px">
+					<Stack direction="row" mb="2.5" mt="1.5" space={3}>
+						<Center
+							size="16"
+							bg="primary.400"
+							rounded="sm"
+							_text={{
+								color: "warmGray.50",
+								fontWeight: "medium",
+							}}
+							shadow={"3"}
+						>
+							<AspectRatio w="100%" ratio={16 / 9}>
+								<Image
+									source={{
+										uri: "https://wallpaperaccess.com/full/317501.jpg",
+									}}
+									alt="Alternate Text"
+									size="xl"
+								/>
+							</AspectRatio>
+						</Center>
+						<Center
+							bg="primary.500"
+							size="16"
+							rounded="sm"
+							_text={{
+								color: "warmGray.50",
+								fontWeight: "medium",
+							}}
+							shadow={"3"}
+						>
+							Box 2
+						</Center>
+					</Stack>
+				</Center>
+				<Divider
+					mb="2"
+					my="2"
+					_light={{
+						bg: "muted.800",
+					}}
+					_dark={{
+						bg: "muted.50",
+					}}
+				/>
+				<Box px="3">
 					<Heading size="md" ml="-1">
-						The Garden City
+						Already submitted trash:
 					</Heading>
 				</Box>
-				<ScrollView w="100%" h="100%" horizontal={true}>
-					<TrashItem />
-				</ScrollView>
-			</Center>
-		</NativeBaseProvider>
+				<Center flex={1} px="3">
+					<ScrollView w="100%" h="100%" horizontal={true} contentContainerStyle={{ flexGrow: 1 }}>
+						<TrashItem />
+					</ScrollView>
+				</Center>
+			</NativeBaseProvider>
+		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	areaView: {
+		flex: 1,
+	},
+});
