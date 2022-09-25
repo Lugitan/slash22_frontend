0b41edbcab4181156d5320238b2a7c3f00b2951e
Davisned <daud.taj@googlemail.com>
2022-09-25 04:38:40 +0200

    Change mapbox to maplibre.

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,9 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
-import mapboxgl from "mapbox-gl";
+import maplibregl from "maplibre-gl";
 import config from "../utils/config";
-
-mapboxgl.accessToken = config.accessToken;
 
 const Root = styled("div")({});
 const MapContainer = styled("div")({
@@ -13,14 +11,16 @@ const MapContainer = styled("div")({
 function MainMap() {
 	const mapContainer = useRef(null);
 	const map = useRef(null);
+	var marker = new maplibregl.Marker({"color": "#FF0000"})
 	const [lng, setLng] = useState(13.404954);
 	const [lat, setLat] = useState(52.520008);
 	const [zoom, setZoom] = useState(9);
+	const [API_KEY] = useState(config.accessToken)
 	useEffect(() => {
 		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
+		map.current = new maplibregl.Map({
 			container: mapContainer.current,
-			style: 'mapbox://styles/mapbox/outdoors-v10?optimize=true', // optimize=true
+			style: `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`, // optimize=true
 			center: [lng, lat],
 			zoom,
 		});
@@ -29,7 +29,13 @@ function MainMap() {
 			setLat(map.current.getCenter().lat.toFixed(4));
 			setZoom(map.current.getZoom().toFixed(2));
 		});
+		map.current.on("click", (e) => {
+			console.log(e)
+			marker.setLngLat(e.lngLat).addTo(map.current);
+		})
+		// new maplibregl.Marker().setLngLat([52.494666, 13.445760]).addTo(map.current);
 	});
+	
 	return (
 		<Root>
 			<MapContainer ref={mapContainer} />
